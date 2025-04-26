cfd4b3f550a27de15ca405c68e4422856c2a54bd
John Doe <johndoe@example.com>
2025-04-26 07:39:44 -0400

    prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import { FolderExplorer, Selected } from './FolderExplorer'
-import { ThumbnailViewer } from './ThumbnailViewer'
+import { useState } from "react";
+import "./App.css";
+import { FolderExplorer, Selected } from "./FolderExplorer";
+import { ThumbnailViewer } from "./ThumbnailViewer";
 
 function App() {
-  const [selected, setSelected] = useState<Selected|null>(null)
-  const [includeSubfolders, setIncludeSubfolders] = useState(false)
+  const [selected, setSelected] = useState<Selected | null>(null);
+  const [includeSubfolders, setIncludeSubfolders] = useState(false);
 
-  return (<div style={{display: 'grid', gridTemplateColumns:'max-content 1fr', gridTemplateRows: '100%', height: '100%', width: '100%'}}>
-        <FolderExplorer onSelect={setSelected} style={{height:"100%", overflowY:"auto"}}/>
-        <ThumbnailViewer directoryPath={selected?.fullPath} includeSubfolders={true} style={{height:"100%", overflowY:'auto'}}/>
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "max-content 1fr",
+        gridTemplateRows: "100%",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <FolderExplorer
+        onSelect={setSelected}
+        style={{ height: "100%", overflowY: "auto" }}
+      />
+      <ThumbnailViewer
+        directoryPath={selected?.fullPath}
+        includeSubfolders={true}
+        style={{ height: "100%", overflowY: "auto" }}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/Media.tsx
+++ client/src/Media.tsx
@@ -1,32 +1,38 @@
+import { memo } from "react";
+
 type Params = {
-    path: string,
-    style?: React.CSSProperties,
-    width?: number,
-}
+  path: string;
+  style?: React.CSSProperties;
+  width?: number;
+};
 
-export const Media:React.FC<Params> = (params)=>{
-    const {path, style, width} = params;
+export const Media: React.FC<Params> = memo((params) => {
+  const { path, style, width } = params;
 
-    const url = new URL(encodeURIComponent(path), `http://localhost:9615/media/`);
-    if (width){
-        url.searchParams.set('width', width.toString());
-    }
+  const url = new URL(encodeURIComponent(path), `http://localhost:9615/media/`);
+  if (width) {
+    url.searchParams.set("width", width.toString());
+  }
 
-    const renderers = [
-        [['jpg','png','jpeg','gif'], () =><img
-                style={{ objectFit: 'contain', ...style }}
-                src={url.toString()}
-                loading="lazy" 
-            />
-        ],
-        [['mp4','mov','avi'],() => 
-            <></>
-        ],
-    ] as const;
+  const renderers = [
+    [
+      ["jpg", "png", "jpeg", "gif"],
+      () => (
+        <img
+          style={{ objectFit: "contain", ...style }}
+          src={url.toString()}
+          loading="lazy"
+        />
+      ),
+    ],
+    [["mp4", "mov", "avi"], () => <></>],
+  ] as const;
 
-    const ext = path.split('.').at(-1) as string;
-    
-    const Renderer = renderers.find(([exts])=>(exts as any as string[]).includes(ext.toLocaleLowerCase()))?.[1];
+  const ext = path.split(".").at(-1) as string;
 
-    return Renderer?<Renderer/> : <div>Unsupported file type</div>;
-}
+  const Renderer = renderers.find(([exts]) =>
+    (exts as any as string[]).includes(ext.toLocaleLowerCase()),
+  )?.[1];
+
+  return Renderer ? <Renderer /> : <div>Unsupported file type</div>;
+});
